db07f0b5a0a70779874df07eb6e08afb8c92b6b4
Julien Fastré <julien@fastre.info>
2012-03-28 17:40:20 +0200

    creation d'un form de login

--- src/SOFFT/AnglaisBundle/Controller/SecurityController.php
+++ src/SOFFT/AnglaisBundle/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace SOFFT\AnglaisBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 /**
  * Description of securityController
@@ -13,7 +14,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class securityController extends Controller {
     
     public function loginAction() {
-        return new Response("login");
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render('SOFFTAnglaisBundle:Security:login.html.twig', array(
+            // last username entered by the user
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        ));
     }
     
     public function login_checkAction() {
