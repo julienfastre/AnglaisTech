9385cb2381279e89184f0e9017b60ef3d2181401
Julien Fastré <julien@fastre.info>
2012-03-30 12:58:22 +0200

    ajoute pagination

--- src/SOFFT/AnglaisBundle/Controller/MotController.php
+++ src/SOFFT/AnglaisBundle/Controller/MotController.php
@@ -15,10 +15,46 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class MotController extends Controller {
     
-    public function listAction($page) {
+    public function listAction(Request $request) {
         $em = $this->getDoctrine()->getEntityManager();
         
-        $list = $em->getRepository('SOFFTAnglaisBundle:Mot')->findAll();
+        //récupère la page courante
+        $page = $request->get('page', NULL);
+        
+        if ($page === NULL) {
+            //il faut reprendre la variable de la session
+            $session = $this->getRequest()->getSession();
+            $page = $session->get('page_list_mot');
+            if ($page === NULL) {
+                $page = 1;
+            }
+        }
+        
+        if ($page < 1) {
+            throw new \Exception("le numéro de page ne peut pas être inférieur à 1");
+        }
+        
+        //stocke la valeur de la page dans la session
+        $this->getRequest()->getSession()->set('page_list_mot', $page);
+        
+        
+        //crée la pagination
+        $mots_par_page = 3;
+        $nb_de_mots = $em->createQuery("SELECT count(m.id) FROM SOFFTAnglaisBundle:Mot m ")->getSingleResult();
+        $page = $page -1;
+        $div = $nb_de_mots/$mots_par_page;
+        $nb_de_page =  round($div, 0);
+        if (($nb_de_mots % $mots_par_page) > 0) {
+            $nb_de_page = $nb_de_page + 1;
+        }
+        
+        $list = $em->getRepository('SOFFTAnglaisBundle:Mot')
+                ->createQueryBuilder('m')
+                ->orderBy('m.en', 'ASC')->orderBy('m.fr', 'ASC')
+                ->setFirstResult($page * $mots_par_page)
+                ->setMaxResults($mots_par_page)
+                ->getQuery()
+                ->getResult();
         
         return $this->render('SOFFTAnglaisBundle:Mot:list.html.twig', array ('list' => $list));
         
