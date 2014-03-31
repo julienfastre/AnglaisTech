91025a1115f9092be64512c3a83405f01cb1744b
Julien Fastré <julien@fastre.info>
2014-03-31 17:17:40 +0200

    pudate app_dev.php and app.php

--- web/app.php
+++ web/app.php
@@ -9,4 +9,10 @@ use Symfony\Component\HttpFoundation\Request;
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
-$kernel->handle(Request::createFromGlobals())->send();
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
--- web/app_dev.php
+++ web/app_dev.php
@@ -18,7 +18,13 @@ require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+Debug::enable();
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
-$kernel->handle(Request::createFromGlobals())->send();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
